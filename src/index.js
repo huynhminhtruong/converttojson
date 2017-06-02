e00208f3a3b78e96513f063a589cc1702025c519
Dave - Truong Huynh Minh <truong.huynh@orientsoftware.net>
2017-06-02 17:02:05 +0700

    Practice ReactJS.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Form from './Form';
+import MyComponent from './MyComponent';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<App />,
+  	document.getElementById('root')
+);
+
+ReactDOM.render(
+	<Form />,
+	document.getElementById('f1')
+);
+
+ReactDOM.render(
+	<MyComponent name="reactjs developer" />,
+	document.getElementById('state')
 );
